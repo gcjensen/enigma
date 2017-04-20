fa2a91e2992fd5809f7d988881d1e12bdf57911c
George <gj4g13@soton.ac.uk>
2017-04-20 10:20:10 +0100

    Actually fixed indentation

--- enigma.js
+++ enigma.js
@@ -10,112 +10,113 @@
 //
 
 function Rotor(settings) {
-	this.base = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
-	// inner is the letters that the outer part
-	// of the rotor maps to
-	this.inner = settings[0].split('');
-	var offset = settings[1];
-	this.turnover = settings[2][0];
-	this.notch = settings[2][1];
-	while (offset--) {
-		this.rotate();
-	}
-	this.advance = false;
+    this.base = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+    // inner is the letters that the outer part
+    // of the rotor maps to
+    this.inner = settings[0].split('');
+    var offset = settings[1];
+    this.turnover = settings[2][0];
+    this.notch = settings[2][1];
+    while (offset--) {
+        this.rotate();
+    }
+    this.advance = false;
 
-	this.rotate = function() {
-		// remove first letter and put it on the end
-		this.base.push(this.base.shift());
-		this.inner.push(this.inner.shift());
+    this.rotate = function() {
+        // remove first letter and put it on the end
+        this.base.push(this.base.shift());
+        this.inner.push(this.inner.shift());
 
-		if (this.base[0] == this.turnover) {
-			this.advance = true;
-		}
-	};
+        if (this.base[0] == this.turnover) {
+            this.advance = true;
+        }
+    };
 
-	// first pass through the rotor
-	this.rightToLeft = function(index) {
-	  return this.base.indexOf(this.inner[index]);
-	};
+    // first pass through the rotor
+    this.rightToLeft = function(index) {
+        return this.base.indexOf(this.inner[index]);
+    };
 
-	// second pass through the rotor
-	this.leftToRight = function(index) {
-	  return this.inner.indexOf(this.base[index]);
-	};
+    // second pass through the rotor
+    this.leftToRight = function(index) {
+        return this.inner.indexOf(this.base[index]);
+    };
 }
 
 function Reflector(wiring) {
-	this.base = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
-	this.inner = wiring.split('');
+    this.base = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+    this.inner = wiring.split('');
 
-	// translates the input letter according to the wiring
-	Reflector.prototype.translate = function(index) {
-		return this.inner.indexOf(this.base[index]);
-	}
+    // translates the input letter according to the wiring
+    Reflector.prototype.translate = function(index) {
+        return this.inner.indexOf(this.base[index]);
+    }
 }
 
 function Plugboard(wiring) {
-	this.base = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
-	this.wiring = wiring;
+    this.base = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+    this.wiring = wiring;
 
-	this.swap = function(index) {
-		// if the giving letter is swapped in the plugboard
-		// it's pair is returned, otherwise nothing happens to it
-		if (this.base[index] in this.wiring) {
-			return this.base.indexOf(this.wiring[this.base[index]]);
-		} else {
-			return index;
-		}
-	}
+    this.swap = function(index) {
+        // if the giving letter is swapped in the plugboard
+        // it's pair is returned, otherwise nothing happens to it
+        if (this.base[index] in this.wiring) {
+            return this.base.indexOf(this.wiring[this.base[index]]);
+        } else {
+            return index;
+        }
+    }
 }
 
 function Enigma(rotors, reflector, plugboard) {
-	this.base = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	this.rotors = rotors
-	this.reflector = reflector
-	this.plugboard = plugboard
+    this.base = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    this.rotors = rotors
+    this.reflector = reflector
+    this.plugboard = plugboard
 
-	this.encipher = function(c) {
-		// each key press causes the rotors to step
-		this.rotors[0].rotate()
+    this.encipher = function(c) {
+        // each key press causes the rotors to step
+        this.rotors[0].rotate()
 
-		// double step if notch has been reached
-		if (this.rotors[1].base[0] == this.rotors[1].notch) {
-			this.rotors[1].rotate()
-		}
+        // double step if notch has been reached
+        if (this.rotors[1].base[0] == this.rotors[1].notch) {
+            this.rotors[1].rotate()
+        }
 
-		// normal stepping
-		for (i = 0; i < this.rotors.length; i++) {
-			if (this.rotors[i].advance) {
-				this.rotors[i].advance = false
-				this.rotors[i + 1].rotate()
-			}
-		}
-		// initially passes through plugboard
-		index = this.plugboard.swap(this.base.indexOf(c))
+        // normal stepping
+        for (i = 0; i < this.rotors.length; i++) {
+            if (this.rotors[i].advance) {
+                this.rotors[i].advance = false
+                this.rotors[i + 1].rotate()
+            }
+        }
 
-		// first pass through each rotor
-		for (i = 0; i < this.rotors.length; i++) {
-			index = this.rotors[i].rightToLeft(index)
-		}
+        // initially passes through plugboard
+        index = this.plugboard.swap(this.base.indexOf(c))
 
-		// translated by the reflector
-		index = this.reflector.translate(index)
+        // first pass through each rotor
+        for (i = 0; i < this.rotors.length; i++) {
+            index = this.rotors[i].rightToLeft(index)
+        }
 
-		// reverse pass through each rotor
-		for (i = this.rotors.length - 1; i >= 0; i--) {
-			index = this.rotors[i].leftToRight(index)
-		}
+        // translated by the reflector
+        index = this.reflector.translate(index)
 
-		// finishes by passing through plugboard again
-		c = this.base[this.plugboard.swap(index)]
+        // reverse pass through each rotor
+        for (i = this.rotors.length - 1; i >= 0; i--) {
+            index = this.rotors[i].leftToRight(index)
+        }
 
-		// change the previous 'lit up' letter back to white
-		document.getElementById(previous).style.background = "#E0E0E0"
-		// make new letter 'light up'
-		document.getElementById(c).style.background = "#FFFF00"
-		previous = c
-		return c
-	}
+        // finishes by passing through plugboard again
+        c = this.base[this.plugboard.swap(index)]
+
+        // change the previous 'lit up' letter back to white
+        document.getElementById(previous).style.background = "#E0E0E0"
+        // make new letter 'light up'
+        document.getElementById(c).style.background = "#FFFF00"
+        previous = c
+        return c
+    }
 }
 
 // keep track of previously pressed letter
@@ -144,5 +145,5 @@ var machine = new Enigma([III, II, I], B, plugboard)
 window.addEventListener("keypress", checkKeyPressed, false);
 
 function checkKeyPressed(e) {
-	machine.encipher(String.fromCharCode(e.charCode).toUpperCase())
+    machine.encipher(String.fromCharCode(e.charCode).toUpperCase())
 }
